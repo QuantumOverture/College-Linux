00381ecd86241bfee44a8fe0258608d5cdff7453
ismaildude <38566647+ismaildude@users.noreply.github.com>
2019-06-19 13:23:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+function CommandEnter(event){
+   var keyCode = event.charCode;
+    if(keyCode == 13){
+        // If Enter Key is pressed:
+        
+        //  Get value of Input tag
+        var CommandLineText = document.getElementById("Command_Line").value;
+        //  Add it on to the old command table at the end
+        document.getElementById("Old_Commands").insertRow(-1).insertCell(-1).innerHTML = CommandLineText;
+        //  Set Input line to empty
+        document.getElementById("Command_Line").value="";
+        
+    }
+    
+}
 
+FocusOnInput = function getFocus(){
+    //  Make sure user is focused on input line
+    document.getElementById("Command_Line").focus();
+}
